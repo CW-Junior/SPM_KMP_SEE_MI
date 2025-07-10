62f68954e5fa105bd9b3de561712a2421cca022c
evanHsu <evanhsu@cw.com.tw>
2025-07-10 17:05:27 +0800

    update packages.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Shared",
-         url: "https://github.com/CW-Junior/SPM_KMP_SEE_MI/releases/download/1.1.0/Shared.xcframework.zip",
+         url: "https://github.com/CW-Junior/SPM_KMP_SEE_MI/releases/download/1.2.0/Shared.xcframework.zip",
          checksum:"49a4eb0a4886a2a6dd6c2664d7111660915c150bd20db8ea8482a349499932ba")
    ]
 )
