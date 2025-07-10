b4bbd022ffec07a381baa293b3016113626ef18a
evanHsu <evanhsu@cw.com.tw>
2025-07-10 16:23:52 +0800

    init package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "https://github.com/CW-Junior/SPM_KMP_SEE_MI/releases/download/1.1.0/Shared.xcframework.zip",
+         checksum:"49a4eb0a4886a2a6dd6c2664d7111660915c150bd20db8ea8482a349499932ba")
+   ]
+)
